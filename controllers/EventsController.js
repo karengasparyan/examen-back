50ef12be2a422c7bd163f874afc9c3156078c1b0
gurgen <gurgenpetroosyan321@gmail.com>
2021-04-06 18:45:07 +0400

    apr06/18:44

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -12,14 +12,19 @@ const  {users, events} = require( "../models").models;
 
 class EventsController {
 
-  static getAllEvents = async (req, res, next) => {
+  static getMyAllEvents = async (req, res, next) => {
     try {
       await validate(req.body, {
         userId: 'required|string',
         page: 'numeric',
       });
 
-      const { userId, page } = req.body;
+      const { userId, query = {}, page = 1, } = req.body;
+
+      console.log(req.body)
+      console.log(req.userId)
+      console.log(req.page)
+      console.log(req.query)
 
       let limit = 20;
       const offset = (page - 1) * limit;
@@ -30,14 +35,42 @@ class EventsController {
         throw HttpError(404);
       }
 
-      const event = await events.find({userId}).skip(offset).limit(limit);
+     const pagesCount = events.count();
+
+      const myEvents = await events.find({userId, ...query}).skip(offset).limit(limit).populate('userId');
 
       await res.json({
         status: 'ok',
-        event,
+        myEvents,
+        myEventPagesCount: Math.ceil(pagesCount.length / limit)
       });
     } catch (e) {
 
+      next(e);
+    }
+  };
+
+  static getAllEvents = async (req, res, next) => {
+    try {
+      await validate(req.body, {
+        page: 'numeric',
+      });
+
+      const { query = {}, page = 1, } = req.body;
+
+      let limit = 20;
+      const offset = (page - 1) * limit;
+
+     const pagesCount = events.count();
+
+      const allEvents = await events.find({ ...query }).skip(offset).limit(limit);
+
+      await res.json({
+        status: 'ok',
+        allEvents,
+        allEventPagesCount: Math.ceil(pagesCount.length / limit)
+      });
+    } catch (e) {
       next(e);
     }
   };
@@ -104,11 +137,149 @@ class EventsController {
 
       const directionRename = await path.join(__dirname, `../public/eventImage/folder_${newEvent.id}`);
 
-      fs.renameSync(direction, directionRename)
+      fs.renameSync(direction, directionRename);
 
       res.json({
         status: 'ok',
         event: newEvent,
+      });
+    } catch (e) {
+
+      next(e);
+    }
+  };
+
+  static updateEvent = async (req, res, next) => {
+    try {
+      console.log(req.body)
+      await validate(req.body, {
+        userId: 'required|string',
+        eventId: 'required|string',
+        title: 'required|string',
+        description: 'required|string',
+        limit: 'required|string',
+        status: 'required',
+        deleteImages: 'array',
+      });
+
+      const { userId, eventId, title, description, limit, status, deleteImages } = req.body;
+      const {files} = req;
+
+      const removeImage = (deleteImages, images) => {
+        let direction = path.join(__dirname, `../public/eventImage/folder_${eventId}`);
+        for (let i = 0; i < deleteImages.length; i++) {
+          const index = images.indexOf(deleteImages[i]);
+          if (index > -1) {
+            images.splice(index, 1);
+            fs.unlinkSync(path.join(direction, deleteImages[i]));
+          }
+        }
+      };
+
+      const user = await users.findById(userId);
+
+      if (!user) {
+        throw HttpError(404, 'invalid user');
+      }
+
+      const images = await events.findById(eventId);
+
+      if (images){
+       await removeImage(deleteImages, images.image);
+      }
+
+      const allowTypes = {
+        'image/jpeg': '.jpg',
+        'image/gif': '.gif',
+        'image/png': '.png',
+      };
+
+      files.forEach(file => {
+        const {mimetype} = file;
+        if (!allowTypes[mimetype]) {
+          throw HttpError(422, 'invalid file type');
+        }
+      });
+
+      const createImage = [...images];
+
+      await Promise.map(files, async (file) => {
+        const ext = allowTypes[file.mimetype];
+        const fileName = `image_${uuid()}${ext}`;
+        fs.writeFileSync(path.join(direction, fileName), file.buffer);
+        createImage.push(fileName);
+      });
+
+      const newEvent = await events.updateOne({eventId}, {
+        title,
+        description,
+        limit,
+        status,
+        image: createImage
+       }
+      );
+
+      res.json({
+        status: 'ok',
+        event: newEvent,
+      });
+    } catch (e) {
+      console.log(e)
+      next(e);
+    }
+  };
+
+  static deleteEvent = async (req, res, next) => {
+    try {
+
+      await validate(req.body, {
+        eventId: 'required|string',
+        userId: 'required|string',
+      });
+
+      const { userId, eventId } = req.body;
+
+      const user = await users.findById(userId);
+
+      const event = await events.findById(eventId);
+
+      if (!user) {
+        throw HttpError(422, 'invalid user');
+      }
+
+      if (!event) {
+        throw HttpError(422, 'invalid event');
+      }
+
+      await events.remove({_id: eventId});
+
+      await res.json({
+        status: 'ok',
+      });
+    } catch (e) {
+
+      next(e);
+    }
+  };
+
+  static singleEvent = async (req, res, next) => {
+    try {
+
+      await validate(req.body, {
+        eventId: 'required|string',
+      });
+
+      const { eventId } = req.body;
+
+      const singleEvent = await events.findById(eventId);
+
+      if (!singleEvent) {
+        throw HttpError(422, 'invalid event');
+      }
+
+      await res.json({
+        status: 'ok',
+        singleEvent,
       });
     } catch (e) {
 
--- routes/events.js
+++ routes/events.js
@@ -5,10 +5,18 @@ import multer, {memoryStorage} from "multer";
 
 const router = express.Router();
 
-router.post('/', authorization, EventsController.getAllEvents);
+router.post('/all-my', authorization, EventsController.getMyAllEvents);
+
+router.post('/all', authorization, EventsController.getAllEvents);
 
 const upload = multer({ storage: memoryStorage() });
 
 router.post('/create', authorization, upload.array('file[]') ,EventsController.createEvent);
+
+router.post('/update', authorization, upload.array('file[]') ,EventsController.updateEvent);
+
+router.post('/delete', authorization, EventsController.deleteEvent);
+
+router.post('/single-event', authorization, EventsController.singleEvent);
 
 export default router;
